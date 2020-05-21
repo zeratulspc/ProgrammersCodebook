fd5df69a9a41a079cf4fccf964d184991abacbf3
민재 김 <zeratulspc@gmail.com>
2020-05-21 12:17:00 +0900

    200521_1

--- Codebook/Codebook/Program.cs
+++ Codebook/Codebook/Program.cs
@@ -11,26 +11,18 @@ namespace Codebook
     {
         static void Main(string[] args)
         {
-            //return 2;
-            Console.WriteLine("answer : "+Network.Network_Solution(3, new int[3, 3] { { 1, 1, 0 }, { 1, 1, 0 }, { 0, 0, 1 } }));
+            ////return 2
+            //Console.WriteLine("answer : "+Network.Network_Solution(3, new int[3, 3] { { 1, 1, 0 }, { 1, 1, 0 }, { 0, 0, 1 } }));
 
-            //return 1;
-            Console.WriteLine("answer : " + Network.Network_Solution(3, new int[3, 3] { { 1, 1, 0 }, { 1, 1, 1 }, { 0, 1, 1 } }));
+            ////return 1
+            //Console.WriteLine("answer : " + Network.Network_Solution(3, new int[3, 3] { { 1, 1, 0 }, { 1, 1, 1 }, { 0, 1, 1 } }));
 
-            //return 3
-            Console.WriteLine("answer : " + Network.Network_Solution(4, new int[4, 4] { { 1, 1, 0, 0 }, { 1, 1, 0, 0 }, { 0, 0, 1, 0 }, { 0, 0, 0, 1} }));
+            //return 4
+            Console.WriteLine("answer : "+WordTransition.Word_solution("hit","cog",new string[] { "hot", "dot", "dog", "lot", "log", "cog"}));
 
-            //return 2
-            Console.WriteLine("answer : " + Network.Network_Solution(4, new int[4, 4] { { 1, 1, 1, 0 }, { 1, 1, 0, 0 }, { 1, 0, 1, 0 }, { 0, 0, 0, 1 } }));
+            //return 0
+            Console.WriteLine("answer : " + WordTransition.Word_solution("hit", "cog", new string[] { "hot", "dot", "dog", "lot", "log"}));
 
-            //return 1
-            Console.WriteLine("answer : " + Network.Network_Solution(4, new int[4, 4] { { 1, 1, 1, 0 }, { 1, 1, 0, 0 }, { 1, 0, 1, 1 }, { 0, 0, 1, 1 } }));
-
-            //return 1
-            Console.WriteLine("answer : " + Network.Network_Solution(4, new int[4, 4] { { 1, 1, 1, 1 }, { 1, 1, 1, 1 }, { 1, 1, 1, 1 }, { 1, 1, 1, 1 } }));
-
-            //return 1
-            Console.WriteLine("answer : " + Network.Network_Solution(4, new int[4, 4] { { 1, 0, 0, 1 }, { 0, 1, 1, 1 }, { 0, 1, 1, 0 }, { 1, 1, 0, 1 } }));
         }
     }
 }
---
+++ Codebook/Codebook/WordTransition.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Codebook
+{
+    class WordTransition
+    {
+        public static int Word_solution(string begin, string target, string[] words)
+        {
+            int answer = 0;
+
+            for (int i = 0; i < words.Length; i++)
+            {
+                int changeCount = 0;
+                Console.WriteLine("[" + words[i] + "]");
+                for (int j = 0; j < words[i].Count(); j++)
+                {
+                    if (!(changeCount > 1))
+                    {
+                        if (begin[j] != words[i][j])
+                        {
+                            Console.Write(words[i][j]);
+                            changeCount++;
+                        }
+                    }
+                }
+                Console.WriteLine("");
+                if (!(changeCount > 1))
+                {
+                    begin = words[i];
+                    if (begin == target)
+                        break;
+                    answer++;
+                }
+            }
+
+            if (!words.Contains(target))
+                answer = 0;
+            return answer;
+        }
+
+    }
+}
